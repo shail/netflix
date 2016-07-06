d73d4b65a142dd5857ecd4a8296be5d56975f8fe
Shail Patel <shailpatel2@gmail.com>
2016-07-06 14:54:30 -0700

    Initial table added

--- app/components/home.js
+++ app/components/home.js
@@ -3,6 +3,7 @@ var _ = require('lodash');
 
 var LineChart = require('/Users/shailpatel/netflix/app/components/lineChart');
 var ShowBarChart = require('/Users/shailpatel/netflix/app/components/barChart');
+var ViewingHistoryTable = require('/Users/shailpatel/netflix/app/components/viewingHistoryTable');
 var React = require('react');
 
 var moment = require('moment');
@@ -25,6 +26,7 @@ var Home = React.createClass({
       <div>
       <ShowBarChart viewingHistory={this.state.viewingHistory} />
       <LineChart viewingHistory={this.state.viewingHistory} width="600" height="480" />
+      <ViewingHistoryTable viewingHistory={this.state.viewingHistory} width = "600" height="480" />
       </div>
     )
   }
---
+++ app/components/viewingHistoryTable.js
@@ -0,0 +1,38 @@
+var _ = require('lodash');
+
+var ReactBsTable = require('react-bootstrap-table');
+var BootstrapTable = ReactBsTable.BootstrapTable;
+var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
+var React = require('react');
+
+var moment = require('moment');
+
+var ViewingHistoryTable = React.createClass({
+  getInitialState: function() {
+    return {
+      data: []
+    };
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    const resultArray = this.state.data;
+    _.forEach(nextProps.viewingHistory.viewing_history, function(object) {
+      const transformedDate = moment(object.date).format('MM-DD-YY');
+      resultArray.push({date: transformedDate, name: object.name});
+    });
+    this.setState({data: resultArray});
+  },
+
+  render: function() {
+    return(
+      <div>
+        <BootstrapTable data={this.state.data} striped={true} hover={true}>
+            <TableHeaderColumn isKey={true} dataSort={true} dataField="date">Date</TableHeaderColumn>
+            <TableHeaderColumn dataSort={true} dataField="name">Name</TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    )
+  },
+});
+
+module.exports = ViewingHistoryTable;
