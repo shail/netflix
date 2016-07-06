c8191e5b5bd41658ddba14114ede935259f2a7e7
Shail Patel <shailpatel2@gmail.com>
2016-07-06 14:02:55 -0700

    Cleanup so we are only making the API request once through parent/child communication

--- app/components/barChart.js
+++ app/components/barChart.js
@@ -18,37 +18,35 @@ var ShowBarChart = React.createClass({
     colors: ["#FF6384", "#36A2EB", "#FFCE56", "#000000", "#00FF00" ]
   },
 
-  componentDidMount: function () {
+  componentWillReceiveProps: function(nextProps) {
     const resultObject = _.zipObject(this.state.data, this.state.labels);
     const resultArray = []
-    this.serverRequest = $.get('http://localhost:4567/viewing_history', function (result) {
-      _.forEach(result.viewing_history, function(object) {
-        const name = object.name;
-        if (name.includes("Season")) {
-          const arrayName = name.split(" ");
-          const editedName = arrayName.slice(0, arrayName.indexOf("Season")).join(" ").replace(/:/, '');
-          resultObject[editedName] == undefined ? resultObject[editedName] = 1 : resultObject[editedName] = resultObject[editedName] + 1;
-        };
-      });
+    _.forEach(nextProps.viewingHistory.viewing_history, function(object) {
+      const name = object.name;
+      if (name.includes("Season")) {
+        const arrayName = name.split(" ");
+        const editedName = arrayName.slice(0, arrayName.indexOf("Season")).join(" ").replace(/:/, '');
+        resultObject[editedName] == undefined ? resultObject[editedName] = 1 : resultObject[editedName] = resultObject[editedName] + 1;
+      };
+    });
 
-      const returnData = [];
-      const returnLabels = [];
+    const returnData = [];
+    const returnLabels = [];
 
-      _.forEach(resultObject, function(value, key) {
-        resultArray.push([value, key]);
-      });
+    _.forEach(resultObject, function(value, key) {
+      resultArray.push([value, key]);
+    });
 
-      const sortedArray = resultArray.sort(function(a, b) {
-        return a[0] - b[0]
-      });
+    const sortedArray = resultArray.sort(function(a, b) {
+      return a[0] - b[0]
+    });
 
-      _.forEach(sortedArray.reverse().slice(0,5), function(pair) {
-        returnData.push(pair[0]);
-        returnLabels.push(pair[1]);
-      });
+    _.forEach(sortedArray.reverse().slice(0,5), function(pair) {
+      returnData.push(pair[0]);
+      returnLabels.push(pair[1]);
+    });
 
-      this.setState({data: returnData, labels: returnLabels});
-    }.bind(this));
+    this.setState({data: returnData, labels: returnLabels});
   },
 
   render: function() {
