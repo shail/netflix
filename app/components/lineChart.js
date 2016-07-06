348aa58bbc7fbb0e7cae12f734180d9683dddec9
Shail Patel <shailpatel2@gmail.com>
2016-07-06 13:56:22 -0700

    Further cleanup

--- app/components/lineChart.js
+++ app/components/lineChart.js
@@ -7,23 +7,26 @@ var React = require('react');
 var moment = require('moment');
 
 var ViewingLineChart = React.createClass({
-  getDefaultProps: function() {
+  getInitialState: function() {
     return {
-      labels: ["January", "February", "March", "April", "May", "June", "July", "August",
-               "September", "October", "November", "December"],
       data: [0,0,0,0,0,0,0,0,0,0,0,0]
     };
   },
 
-  transform: function(data) {
-    if (data != undefined) {
-      const result_array = this.props.data;
-      _.forEach(data.viewing_history, function(object) {
-        const month = moment(object.date).get('month');
-        result_array[month] = result_array[month] + 1;
-      });
-      return result_array
-    }
+  getDefaultProps: function() {
+    return {
+      labels: ["January", "February", "March", "April", "May", "June", "July", "August",
+               "September", "October", "November", "December"],
+    };
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    const resultArray = this.state.data;
+    _.forEach(nextProps.viewingHistory.viewing_history, function(object) {
+      const month = moment(object.date).get('month');
+      resultArray[month] = resultArray[month] + 1;
+    });
+    this.setState({data: resultArray});
   },
 
   render: function() {
@@ -35,7 +38,7 @@ var ViewingLineChart = React.createClass({
             strokeColor : "#ACC26D",
             pointColor : "#fff",
             pointStrokeColor : "#9DB86D",
-            data: this.transform(this.props.viewingHistory),
+            data: this.state.data,
           }
         ]
     };
